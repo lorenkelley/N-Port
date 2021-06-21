3ad4c8277643838fb39b0c985dbee12a7fe365a5
loren kelley <kelley.loren52@gmail.com>
2021-06-21 02:50:40 -0400

    added functionality for menu and transparent nav

---
+++ script.js
@@ -0,0 +1,28 @@
+//code for hamburger menu and links to function when clicked and scrolled
+const ham_menu = document.querySelector('.head .navbar .navlink .menu');
+const mobile_menu = document.querySelector('.head .navbar .navlink ul');
+const menu_item = document.querySelectorAll('.head .navbar .navlink ul li a');
+const head = document.querySelector('.head.container');
+
+ham_menu.addEventListener('click', () => {
+	ham_menu.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+//gives menu color if scrolled down page and makes it transparent if not
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		head.style.backgroundColor = 'pink';
+	} else {
+		head.style.backgroundColor = 'transparent';
+	}
+});
+
+//returns list of links and adds thems to menu when clicked
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		ham_menu.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
